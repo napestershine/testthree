1db38450b2c0346f3bb5d17c24a1e9add91b4619
Manoj <napestershine@gmail.com>
2016-04-06 09:41:21 +0530

    bundle started

---
+++ src/Brooter/ForumBundle/BrooterForumBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brooter\ForumBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BrooterForumBundle extends Bundle
+{
+}
---
+++ src/Brooter/UserBundle/BrooterUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brooter\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BrooterUserBundle extends Bundle
+{
+}
